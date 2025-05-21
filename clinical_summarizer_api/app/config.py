3bb35f66a78727d521b650260391fff9db96914d
smallel4 <smallel4@cougarnet.uh.edu>
2025-05-20 21:23:46 -0500

    tracing back by removing unnecessary files

--- clinical_summarizer_api/app/config.py
+++
@@ -1,3 +0,0 @@
-DATABASE_URL = "postgresql://hera:hera123@localhost:5432/hera_ai"
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # hera-healthcare-ai/
-MODEL_PATH = os.path.join(BASE_DIR, "model")
--- clinical_summarizer_api/app/logger.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("clinical_summarizer_api")
-
